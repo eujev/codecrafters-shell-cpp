3982beefd1719cccd4c547010c1b7fab5ea098a3
eujev <87488490+eujev@users.noreply.github.com>
2024-12-03 01:31:38 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
- 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found\n";
+    // Uncomment this block to pass the first stage
+    while(true) {
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+        std::cout << input << ": command not found\n";
+    }
 }
