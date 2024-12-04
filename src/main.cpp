39e5e9c4399d80e0be8b4000862daf39ae0da022
eujev <87488490+eujev@users.noreply.github.com>
2024-12-04 01:45:09 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,17 @@ int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    int exit = 1;
     // Uncomment this block to pass the first stage
-    while(exit != 0) {
+    while(true) {
         std::cout << "$ ";
         std::string input;
         std::getline(std::cin, input);
         if (input == "exit 0") {
-            exit = 0;
+            return 0;
+        }
+        std::string command = input.substr(0, input.find(" "));
+        if (command == "echo") {
+            std::cout << input.substr(input.find(" ") + 1, input.length()) << "\n";
         }
         else {
             std::cout << input << ": command not found\n";
