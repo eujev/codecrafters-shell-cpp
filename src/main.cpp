84e10b7f291ab99075bdcd62c8df2df64418882a
eujev <87488490+eujev@users.noreply.github.com>
2024-12-05 02:01:44 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+void function_type(std::string command_input);
+
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
@@ -13,11 +15,26 @@ int main() {
             return 0;
         }
         std::string command = input.substr(0, input.find(" "));
+        std::string command_input = input.substr(input.find(" ") + 1, input.length()); 
         if (command == "echo") {
-            std::cout << input.substr(input.find(" ") + 1, input.length()) << "\n";
+            std::cout << command_input << "\n";
+        }
+        else if (command == "type") {
+            function_type(command_input);
         }
         else {
             std::cout << input << ": command not found\n";
         }
     }
 }
+
+void function_type(std::string command_input)
+{
+    if (command_input == "echo" || command_input == "type" || command_input == "exit") {
+        std::cout << command_input << " is a shell builtin\n";
+    }
+    else {
+        std::cout << command_input << ": not found\n";
+    }
+}
+
