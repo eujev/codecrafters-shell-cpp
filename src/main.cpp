1c7535c0c61e7cff197bb09413c927f1cc1a3cc2
eujev <87488490+eujev@users.noreply.github.com>
2024-12-08 01:52:18 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include <cstddef>
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
 
 void function_type(std::string command_input);
+std::vector<std::string> split(std::string s, std::string delimeter);
 
 int main() {
     // Flush after every std::cout / std:cerr
@@ -30,11 +36,35 @@ int main() {
 
 void function_type(std::string command_input)
 {
+    std::string path = std::getenv("PATH");
+    std::vector<std::string> split_paths = split(path, ":");
+
     if (command_input == "echo" || command_input == "type" || command_input == "exit") {
         std::cout << command_input << " is a shell builtin\n";
+        return;
     }
-    else {
-        std::cout << command_input << ": not found\n";
+    for(auto it = split_paths.begin(); it != split_paths.end(); ++it) {
+        for (const auto & entry : std::filesystem::directory_iterator(*it)) {
+            if (command_input == entry.path().filename().string()) {
+                std::cout << command_input << " is " << entry.path().string() << "\n";
+                return;
+            }
+        }
     }
+    std::cout << command_input << ": not found\n";
 }
 
+std::vector<std::string> split(std::string s, std::string delimeter)
+{
+    std::vector<std::string> split_strings;
+    size_t pos = 0;
+    std::string token;
+    while((pos = s.find(delimeter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        split_strings.push_back(token);
+        s.erase(0, pos + delimeter.length());
+    }
+    split_strings.push_back(s);
+
+    return split_strings;
+}
