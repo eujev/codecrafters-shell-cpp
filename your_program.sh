4f0f627d62253d20a4438f84113b8752c7bcab86
eujev <87488490+eujev@users.noreply.github.com>
2025-03-06 00:13:16 +0100

    Change files for local use

--- your_program.sh
+++ your_program.sh
@@ -14,12 +14,12 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
+  cmake -B build_local -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake --build ./build_local
 )
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec ./build/shell "$@"
+exec ./build_local/shell "$@"
