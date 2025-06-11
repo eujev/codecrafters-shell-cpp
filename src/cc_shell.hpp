16386c1633ca17d8f65a1aa9e7bae035785bb940
eujev <87488490+eujev@users.noreply.github.com>
2025-06-11 23:49:31 +0200

    Refactor by adding new files for shell class

---
+++ src/cc_shell.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "iostream"
+#include "vector"
+
+class CC_Shell {
+public:
+  CC_Shell();
+  void get_non_can_input(std::string &input);
+  bool handle_tab(std::string &input, bool double_tap);
+  void handle_command(std::string command, std::string command_args);
+  void command_echo(std::string command_args);
+  void command_type(std::string command_args);
+  void command_execute(std::string command, std::string command_args);
+  void command_pwd();
+  void command_cd(std::string command_args);
+
+private:
+  std::vector<std::string> builtin_commands{"echo", "type", "exit", "pwd",
+                                            "cd"};
+  std::string get_path(std::string command);
+  std::string path_env;
+};
